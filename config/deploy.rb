701260369de2e889300fac642f40b01b1b4982bc
Winston Kotzan <wak@wakproductions.com>
2017-06-03 16:48:51 -0400

    Docker config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,5 @@ set :deploy_to, '/var/www/ycurve'
 
 set :repo_url, 'git@github.com:wakproductions/ycurve.git'
 set :scm, :git
+
+set :docker_container_name, 'ycurve'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,16 @@
 server 'wak-ustreasuryyieldcurve-production', user: 'ubuntu', roles: %w{web app db}
+
+namespace :custom do
+  task :setup_container do
+    on roles(:web) do |host|
+      puts "================Starting Docker setup===================="
+      execute "cd #{deploy_to}/current"
+      execute "docker stop #{fetch(:docker_container_name)}; echo 0"
+      execute "docker rm -fv #{fetch(:docker_container_name)}; echo 0"
+      execute 'docker-compose build && docker-compose up'
+      execute "docker exec -it #{fetch(:docker_container_name)} bundle exec rake db:migrate"
+    end
+  end
+end
+
+after "deploy:finishing", "custom:setup_container"
