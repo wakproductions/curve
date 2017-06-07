d007419728f41a273a3af8c7b7da76b1c74808a6
Winston Kotzan <wak@wakproductions.com>
2017-06-06 23:17:38 -0400

    Timezone Javascript bug in displaying date

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,12 +4,13 @@ namespace :custom do
   task :setup_container do
     on roles(:web) do |host|
       puts "================Starting Docker setup===================="
-      execute "sudo docker stop $(sudo docker ps -q)"
-      execute "sudo docker rm $(docker ps -a | grep #{fetch(:docker_container_name)} | awk \"{print \$1}\")"
-
       # Working directory hack
       # https://stackoverflow.com/questions/19452983/capistrano-3-execute-within-a-directory
       execute "cd #{fetch(:deploy_to)}/current; sudo docker-compose build"
+
+      execute "sudo docker stop $(sudo docker ps -q)"
+      execute "sudo docker rm $(docker ps -a | grep #{fetch(:docker_container_name)} | awk \"{print \$1}\")"
+
       execute "cd #{fetch(:deploy_to)}/current; sudo docker-compose up -d"
 
       # Run the daemons
